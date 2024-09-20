3b61f7b11aa8b48c8fb86946ba1f7fe7e822b80a
FiveStarK <gbkos@naver.com>
2024-09-20 19:18:07 +0900

    complete

--- calculator.py
+++ calculator.py
@@ -1,31 +1,37 @@
 def add(a, b):
-    pass
+    return a + b
 
 
 def subtract(a, b):
-	pass
+	return a -b
 
 
 def multiply(a, b):
-    pass
+    return a * b
 
 
 def divide(a, b):
-    pass
+    return a / b
 
 
 def pow(a, b):
-    pass
+    return a ** b
 
 
 def abs(a):
-    pass
+    return a if a >= 0 else -a
 
 
 def mod(a, b):
-    pass
+    return a % b
 
 
 if __name__ == "__main__":
     # 간단한 테스트 코드
-    pass
+    print("Add: ", add(5, 3))
+    print("Subtract: ", subtract(5, 3))
+    print("Multiply: ", multiply(5, 3))
+    print("Divide: ", divide(5, 3))
+    print("Power: ", pow(5, 3))
+    print("Absolute: ", abs(-5))
+    print("Modulus: ", mod(5, 3))
